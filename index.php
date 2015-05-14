2f49675406a8c088428e2d2df9438b70abf3fb72
Michael <mpetrosky99@gmail.com>
2015-05-13 22:00:39 -0400

    Add index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+<title> Test</title>
+<?php
+#$servername = "localhost";
+#$dbname = "autorepair";
+
+// Create connection
+#$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+#if ($conn->connect_error) {
+#    die("Connection failed: " . $conn->connect_error);
+#}
+#echo "Connected successfully";
+#echo "My first PHP script!";
+echo "<a href=GetReport.php>Get Report</a><br>";
+echo
+
+#$sql = "select * from vehicle";
+#$result = $conn->query($sql);
+#echo gettype($conn);
+#echo gettype($result);
+#$result2 = mysql_query($sql);
+
+
+#$result = $conn->query($sql);
+#echo $result;
+$conn->close();
+?>
+</table>
+</body>
+</html> 
